4dc83253ab3d0e24e057eb76881ba2ff523ecfac
Kai Tamkun <ktamkun@ucsc.edu>
2022-04-19 01:06:57 -0700

    Load plugins on startup

--- src/Core.cpp
+++ src/Core.cpp
@@ -8,6 +8,11 @@ namespace Algiz {
 	Server * run(const Options &options) {
 		auto *server = new HTTP::Server(options);
 		INFO("Binding to " << options.ip << " on port " << options.port << ".");
+
+		server->loadPlugins("./plugin");
+		server->preinitPlugins();
+		server->postinitPlugins();
+
 		server->messageHandler = [server](int client, const std::string &message) {
 			try {
 				server->getClients().at(client)->handleInput(message);
