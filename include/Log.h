3bfc5f36203b5b4a6109f44dbc579e96500df580
Kai Tamkun <ktamkun@ucsc.edu>
2022-05-04 22:53:41 -0700

    Try unsuccessfully to fix "The futex facility returned an unexpected error code."

--- include/Log.h
+++ include/Log.h
@@ -6,6 +6,8 @@
 
 namespace Algiz {
 	struct Logger {
+		std::mutex mutex;
+
 		template <typename T>
 		Logger & operator<<(const T &object) {
 			std::cerr << object;
@@ -31,31 +33,28 @@ namespace Algiz {
 	};
 
 	extern Logger log;
-	extern std::mutex logMutex;
 }
 
-
-
 #define INFO(message) \
-	do { std::unique_lock lock(Algiz::logMutex); \
+	do { std::unique_lock lock(Algiz::log.mutex); \
 	     ::Algiz::log << "\e[2m[\e[1m" << ::Algiz::Logger::getTimestamp() \
 	                  << "\e[22;2m]\e[22m (\e[22;1;34mi\e[22;39m)\e[2m ::\e[22m " \
 	                  << message << std::endl; } while (false)
 
 #define WARN(message) \
-	do { std::unique_lock lock(Algiz::logMutex); \
+	do { std::unique_lock lock(Algiz::log.mutex); \
 	     ::Algiz::log << "\e[2m[\e[1m" << ::Algiz::Logger::getTimestamp() \
 	                  << "\e[22;2m]\e[22m (\e[22;1;33m!\e[22;39m)\e[2m ::\e[22m " \
 	                  << message << std::endl; } while (false)
 
 #define ERROR(message) \
-	do { std::unique_lock lock(Algiz::logMutex); \
+	do { std::unique_lock lock(Algiz::log.mutex); \
 	     ::Algiz::log << "\e[2m[\e[1m" << ::Algiz::Logger::getTimestamp() \
 	                  << "\e[22;2m]\e[22m (\e[22;1;31m!\e[22;39m)\e[2m ::\e[22m " \
 	                  << message << std::endl; } while (false)
 
 #define SPAM(message) \
-	do { std::unique_lock lock(Algiz::logMutex); \
+	do { std::unique_lock lock(Algiz::log.mutex); \
 	     ::Algiz::log << "\e[2m[\e[1m" << ::Algiz::Logger::getTimestamp() \
 	                  << "\e[22;2m]\e[22m (\e[22;1;35m_\e[22;39m)\e[2m :: " \
 	                  << message << "\e[22m" << std::endl; } while (false)
