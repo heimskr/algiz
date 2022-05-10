3b78199384e00b3cbe9551f4951e883b989b7b13
Kai Tamkun <ktamkun@ucsc.edu>
2022-05-09 21:20:24 -0700

    Support POST in HttpFileserv

--- include/plugins/HttpFileserv.h
+++ include/plugins/HttpFileserv.h
@@ -13,7 +13,6 @@
 
 namespace Algiz::HTTP {
 	class Client;
-	class Server;
 }
 
 namespace Algiz::Plugins {
@@ -23,21 +22,28 @@ namespace Algiz::Plugins {
 			size_t chunkSize = 1 << 24;
 
 			[[nodiscard]] std::string getName()        const override { return "HTTP Fileserv"; }
-			[[nodiscard]] std::string getDescription() const override {
-				return "Serves files from the filesystem over HTTP.";
-			}
+			[[nodiscard]] std::string getDescription() const override { return "Serves files over HTTP."; }
 			[[nodiscard]] std::string getVersion()     const override { return "0.0.1"; }
 
 			void postinit(PluginHost *) override;
 			void cleanup(PluginHost *) override;
 
-			std::shared_ptr<PluginHost::PreFn<HTTP::Server::HandlerArgs &>> handler =
-				std::make_shared<PluginHost::PreFn<HTTP::Server::HandlerArgs &>>(bind(*this, &HttpFileserv::handle));
+			std::shared_ptr<PluginHost::PreFn<HTTP::Server::HandlerArgs &>> getHandler =
+				std::make_shared<PluginHost::PreFn<HTTP::Server::HandlerArgs &>>(bind(*this, &HttpFileserv::handleGET));
+
+			std::shared_ptr<PluginHost::PreFn<HTTP::Server::HandlerArgs &>> postHandler =
+				std::make_shared<PluginHost::PreFn<HTTP::Server::HandlerArgs &>>(bind(*this,
+					&HttpFileserv::handlePOST));
 
 		private:
+			Plugins::CancelableResult handleGET(HTTP::Server::HandlerArgs &, bool not_disabled);
+			Plugins::CancelableResult handlePOST(HTTP::Server::HandlerArgs &, bool not_disabled);
 
-			Plugins::CancelableResult handle(HTTP::Server::HandlerArgs &, bool not_disabled);
+			bool authFailed(HTTP::Server::HandlerArgs &, const std::filesystem::path &) const;
+			bool findPath(std::filesystem::path &) const;
+			void serveRange(HTTP::Server::HandlerArgs &, const std::filesystem::path &) const;
+			void serveFull(HTTP::Server::HandlerArgs &, const std::filesystem::path &) const;
 
-			std::vector<std::string> getDefaults();
+			std::vector<std::string> getDefaults() const;
 	};
 }
--- src/plugins/httpfileserv/HttpFileserv.cpp
+++ src/plugins/httpfileserv/HttpFileserv.cpp
@@ -14,14 +14,16 @@
 namespace Algiz::Plugins {
 	void HttpFileserv::postinit(PluginHost *host) {
 		auto &http = dynamic_cast<HTTP::Server &>(*(parent = host));
-		http.getHandlers.push_back(std::weak_ptr(handler));
+		http.getHandlers.push_back(std::weak_ptr(getHandler));
+		http.postHandlers.push_back(std::weak_ptr(postHandler));
 	}
 
 	void HttpFileserv::cleanup(PluginHost *host) {
-		PluginHost::erase(dynamic_cast<HTTP::Server &>(*host).getHandlers, std::weak_ptr(handler));
+		PluginHost::erase(dynamic_cast<HTTP::Server &>(*host).getHandlers, std::weak_ptr(getHandler));
+		PluginHost::erase(dynamic_cast<HTTP::Server &>(*host).postHandlers, std::weak_ptr(postHandler));
 	}
 
-	CancelableResult HttpFileserv::handle(HTTP::Server::HandlerArgs &args, bool not_disabled) {
+	CancelableResult HttpFileserv::handleGET(HTTP::Server::HandlerArgs &args, bool not_disabled) {
 		if (!not_disabled)
 			return CancelableResult::Pass;
 
@@ -35,6 +37,76 @@ namespace Algiz::Plugins {
 			return CancelableResult::Pass;
 		}
 
+		if (authFailed(args, full_path))
+			return CancelableResult::Kill;
+
+		if (!findPath(full_path))
+			return CancelableResult::Pass;
+
+		try {
+			const auto extension = full_path.extension();
+			if (extension == ".t") {
+				http.server->send(client.id,
+					HTTP::Response(200, renderTemplate(readFile(full_path))).setMIME("text/html"));
+			} else if (!request.ranges.empty() || request.suffixLength != 0) {
+				serveRange(args, full_path);
+			} else {
+				serveFull(args, full_path);
+			}
+			client.close();
+			return CancelableResult::Approve;
+		} catch (const std::exception &err) {
+			ERROR(err.what());
+			client.send(HTTP::Response(403, "Forbidden"));
+			client.close();
+			return CancelableResult::Approve;
+		}
+	}
+
+	CancelableResult HttpFileserv::handlePOST(HTTP::Server::HandlerArgs &args, bool not_disabled) {
+		if (!not_disabled)
+			return CancelableResult::Pass;
+
+		auto &[http, client, request, parts] = args;
+		auto full_path = (http.webRoot / ("./" + unescape(request.path, true))).lexically_normal();
+		if (std::filesystem::is_directory(full_path))
+			full_path /= "";
+
+		if (!isSubpath(http.webRoot, full_path)) {
+			ERROR("Not subpath of " << http.webRoot << ": " << full_path);
+			return CancelableResult::Pass;
+		}
+
+		if (authFailed(args, full_path))
+			return CancelableResult::Kill;
+
+		if (!findPath(full_path))
+			return CancelableResult::Pass;
+
+		try {
+			const auto extension = full_path.extension();
+			if (extension == ".t") {
+				http.server->send(client.id,
+					HTTP::Response(200, renderTemplate(readFile(full_path), {
+						{"post", nlohmann::json(request.postParameters).dump()}
+					})).setMIME("text/html"));
+			} else if (!request.ranges.empty() || request.suffixLength != 0) {
+				serveRange(args, full_path);
+			} else {
+				serveFull(args, full_path);
+			}
+			client.close();
+			return CancelableResult::Approve;
+		} catch (const std::exception &err) {
+			ERROR(err.what());
+			client.send(HTTP::Response(403, "Forbidden"));
+			client.close();
+			return CancelableResult::Approve;
+		}
+	}
+
+	bool HttpFileserv::authFailed(HTTP::Server::HandlerArgs &args, const std::filesystem::path &full_path) const {
+		auto &[http, client, request, parts] = args;
 		auto dir = full_path.parent_path();
 		const auto root = dir.root_path();
 		do {
@@ -47,11 +119,11 @@ namespace Algiz::Plugins {
 					if (auth_result == HTTP::AuthenticationResult::Missing) {
 						const std::string realm = auth->contains("realm")? auth->at("realm") : "";
 						http.send401(client, realm);
-						return CancelableResult::Approve;
+						return true;
 					}
 					if (auth_result != HTTP::AuthenticationResult::Success) {
 						http.send401(client);
-						return CancelableResult::Approve;
+						return true;
 					}
 					break;
 				} catch (const nlohmann::detail::out_of_range &) {
@@ -65,9 +137,13 @@ namespace Algiz::Plugins {
 
 		if (full_path.filename() == ".algiz") {
 			http.send401(client);
-			return CancelableResult::Approve;
+			return true;
 		}
 
+		return false;
+	}
+
+	bool HttpFileserv::findPath(std::filesystem::path &full_path) const {
 		if (std::filesystem::is_regular_file(full_path)) {
 			// Use the path as-is.
 		} else {
@@ -82,77 +158,49 @@ namespace Algiz::Plugins {
 			}
 
 			if (!found)
-				return CancelableResult::Pass;
+				return false;
 		}
 
-		try {
-			const auto extension = full_path.extension();
-			if (extension == ".t") {
-				http.server->send(client.id,
-					HTTP::Response(200, renderTemplate(readFile(full_path))).setMIME("text/html"));
-			} else if (!request.ranges.empty() || request.suffixLength != 0) {
-				const size_t filesize = std::filesystem::file_size(full_path);
-				if (request.valid(filesize)) {
-					std::ifstream stream;
-					stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-					stream.open(full_path);
-					stream.exceptions(std::ifstream::goodbit);
-					if (!stream.is_open()) {
-						http.send403(client);
-					} else {
-						HTTP::Response response(206, "");
+		return true;
+	}
 
-						size_t length = request.suffixLength;
-						for (const auto &[start, end]: request.ranges)
-							length += end - start + 1;
-
-						response.setAcceptRanges();
-						response["Content-Length"] = std::to_string(length);
-						response.setLastModified(lastWritten(full_path));
-
-						http.server->send(client.id, response.noContent());
-						for (const auto &[start, end]: request.ranges) {
-							size_t remaining = end - start + 1;
-							stream.seekg(start, std::ios::beg);
-							auto buffer = std::make_unique<char[]>(std::min(chunkSize, remaining));
-							while (0 < remaining) {
-								const size_t to_read = std::min(chunkSize, remaining);
-								stream.read(buffer.get(), to_read);
-								http.server->send(client.id, std::string_view(buffer.get(), to_read));
-								remaining -= to_read;
-							}
-						}
-
-						if (request.suffixLength != 0) {
-							size_t remaining = request.suffixLength;
-							stream.seekg(filesize - request.suffixLength, std::ios::beg);
-							auto buffer = std::make_unique<char[]>(std::min(chunkSize, remaining));
-							while (0 < remaining) {
-								const size_t to_read = std::min(chunkSize, remaining);
-								stream.read(buffer.get(), to_read);
-								http.server->send(client.id, std::string_view(buffer.get(), to_read));
-								remaining -= to_read;
-							}
-						}
-					}
-				} else
-					http.send400(client);
+	void HttpFileserv::serveRange(HTTP::Server::HandlerArgs &args, const std::filesystem::path &full_path) const {
+		auto &[http, client, request, parts] = args;
+		const size_t filesize = std::filesystem::file_size(full_path);
+		if (request.valid(filesize)) {
+			std::ifstream stream;
+			stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+			stream.open(full_path);
+			stream.exceptions(std::ifstream::goodbit);
+			if (!stream.is_open()) {
+				http.send403(client);
 			} else {
-				std::ifstream stream;
-				stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-				stream.open(full_path);
-				stream.exceptions(std::ifstream::goodbit);
-				if (!stream.is_open()) {
-					http.send403(client);
-				} else {
-					const std::string mime = getMIME(full_path.extension());
-					const size_t filesize = std::filesystem::file_size(full_path);
-					HTTP::Response response(200, "");
-					response.setLastModified(lastWritten(full_path)).setAcceptRanges().setMIME(mime);
-					response["Content-Length"] = std::to_string(filesize);
-					http.server->send(client.id, response.noContent());
-					size_t remaining = filesize;
-					stream.seekg(0, std::ios::beg);
+				HTTP::Response response(206, "");
+
+				size_t length = request.suffixLength;
+				for (const auto &[start, end]: request.ranges)
+					length += end - start + 1;
+
+				response.setAcceptRanges();
+				response["Content-Length"] = std::to_string(length);
+				response.setLastModified(lastWritten(full_path));
+
+				http.server->send(client.id, response.noContent());
+				for (const auto &[start, end]: request.ranges) {
+					size_t remaining = end - start + 1;
+					stream.seekg(start, std::ios::beg);
+					auto buffer = std::make_unique<char[]>(std::min(chunkSize, remaining));
+					while (0 < remaining) {
+						const size_t to_read = std::min(chunkSize, remaining);
+						stream.read(buffer.get(), to_read);
+						http.server->send(client.id, std::string_view(buffer.get(), to_read));
+						remaining -= to_read;
+					}
+				}
+
+				if (request.suffixLength != 0) {
+					size_t remaining = request.suffixLength;
+					stream.seekg(filesize - request.suffixLength, std::ios::beg);
 					auto buffer = std::make_unique<char[]>(std::min(chunkSize, remaining));
 					while (0 < remaining) {
 						const size_t to_read = std::min(chunkSize, remaining);
@@ -162,17 +210,38 @@ namespace Algiz::Plugins {
 					}
 				}
 			}
-			client.close();
-			return CancelableResult::Approve;
-		} catch (std::exception &err) {
-			ERROR(err.what());
-			client.send(HTTP::Response(403, "Forbidden"));
-			client.close();
-			return CancelableResult::Approve;
+		} else
+			http.send400(client);
+	}
+
+	void HttpFileserv::serveFull(HTTP::Server::HandlerArgs &args, const std::filesystem::path &full_path) const {
+		auto &[http, client, request, parts] = args;
+		std::ifstream stream;
+		stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		stream.open(full_path);
+		stream.exceptions(std::ifstream::goodbit);
+		if (!stream.is_open()) {
+			http.send403(client);
+		} else {
+			const std::string mime = getMIME(full_path.extension());
+			const size_t filesize = std::filesystem::file_size(full_path);
+			HTTP::Response response(200, "");
+			response.setLastModified(lastWritten(full_path)).setAcceptRanges().setMIME(mime);
+			response["Content-Length"] = std::to_string(filesize);
+			http.server->send(client.id, response.noContent());
+			size_t remaining = filesize;
+			stream.seekg(0, std::ios::beg);
+			auto buffer = std::make_unique<char[]>(std::min(chunkSize, remaining));
+			while (0 < remaining) {
+				const size_t to_read = std::min(chunkSize, remaining);
+				stream.read(buffer.get(), to_read);
+				http.server->send(client.id, std::string_view(buffer.get(), to_read));
+				remaining -= to_read;
+			}
 		}
 	}
 
-	std::vector<std::string> HttpFileserv::getDefaults() {
+	std::vector<std::string> HttpFileserv::getDefaults() const {
 		if (config.contains("default")) {
 			const auto &defaults = config.at("default");
 			if (defaults.is_string())
