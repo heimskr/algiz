9960e5ba736d47b2d000e038e947751d2cd594c9
Kai Tamkun <kai@tamkun.io>
2025-06-07 04:21:27 -0700

    Fix .aliases.sh

--- .aliases.sh
+++ .aliases.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-BUILDDIR="${GAME3_BUILDDIR:-builddir}"
+BUILDDIR="${ALGIZ_BUILDDIR:-builddir}"
 b() { cd $BUILDDIR && { ninja; cd ..; }; }
 alias reconf="CXX='ccache clang++' CC='ccache clang' CXX_LD='mold' meson setup --reconfigure $BUILDDIR ."
-alias ts="./$BUILDDIR/src/game3 -s"
-alias t="./$BUILDDIR/src/game3"
+alias t="./$BUILDDIR/src/algiz"
 bts() { b && ts; }
 bt() { b && t; }
